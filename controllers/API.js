b2e9b5ae32b74e11c510d6dbd622c825c7cb313a
Kyle Kleven <kylekleven8@gmail.com>
2021-07-21 23:28:29 -0500

    Added api/books

--- controllers/API.js
+++
@@ -1,9 +0,0 @@
-
-
-// `/api/books` (get) - Should return all saved books as JSON.
-
-// * `/api/books` (post) - Will be used to save a new book to the database.
-
-// * `/api/books/:id` (delete) - Will be used to delete a book from the database by Mongo `_id`.
-
-// * `*` (get) - Will load your single HTML page in `client/build/index.html`. Make sure you have this _after_ all other routes are defined.
